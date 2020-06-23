e256adb74712be41030252e7ea7f9c6395880806
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2020-06-23 14:46:05 -0700

    add Joins

---
+++ HackerRank/Basic Join/AvgPopulationOfEachContinent.sql
@@ -0,0 +1,16 @@
+Given the CITY
+and COUNTRY tables, query the names of all the continents
+(COUNTRY.Continent) 
+and their respective average city populations
+(CITY.Population) rounded down to the nearest integer.
+
+Note:
+CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+--ANSWER--
+
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population))
+FROM CITY
+    INNER JOIN COUNTRY
+    ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent
