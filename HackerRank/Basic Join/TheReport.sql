7bdaa9a43229ebdadf08278adb6cf000151188c3
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2020-06-23 15:59:31 -0700

    HackerRank Medium

---
+++ HackerRank/Basic Join/TheReport.sql
@@ -0,0 +1,17 @@
+Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. 
+Ketty doesnt want the NAMES of those students who received a grade lower than 8.
+The report must be in descending order by grade -- i.e. higher grades are entered first.
+If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. 
+Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. 
+If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+Write a query to help Eve.
+
+--ANSWER--
+
+SELECT IF (g.GRADE < 8, 'NULL', s.NAME), g.Grade, s.Marks 
+FROM Students AS s 
+JOIN Grades AS g 
+ON s.Marks 
+BETWEEN g.Min_Mark AND g.Max_Mark
+ORDER BY g.GRADE DESC, s.NAME, s.MARKS;
