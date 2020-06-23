d2d9933297931d9cedac80b23be57aec17b44ef7
jflopezr11 <joshualopezroque@gmail.com>
2020-06-22 19:45:56 -0700

    Commit

---
+++ HackerRank/EmployeeName.sql
@@ -0,0 +1,5 @@
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+--ANSWER--
+
+SELECT NAME FROM Employee ORDER BY NAME ASC;
